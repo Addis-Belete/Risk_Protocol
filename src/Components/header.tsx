0237b04cbd12bfde3effe8d73d3eb2251bb542f0
Addis Belete <addis.bel@gmail.com>
2023-02-25 22:13:59 +0300

    Add inputs in index.tsx

--- src/Components/header.tsx
+++ src/Components/header.tsx
@@ -1,18 +1,19 @@
 import styles from "@/styles/Header.module.css";
+import Link from 'next/link'
 export const Header = () => {
   return (
-    <div >
+    <div>
       <nav className={styles.header}>
-        <h1>RISK EXCHANGE</h1>
+        <h1><Link href="/">RISK EXCHANGE</Link></h1>
         <ul>
-          <li>List Order</li>
-          <li>Buy Order</li>
-          <li>Rebase</li>
+          <li><Link href="/">List Order</Link></li>
+          <li><Link href="/listOrders">Buy Order</Link></li>
+          <li>< Link href="/Rebase">Rebase</Link></li>
         </ul>
- <div>
-<button>Connect</button>
-<span>0x0000000000</span>
-</div>
+        <div>
+          <button>Connect</button>
+          <span>0x0000000000</span>
+        </div>
       </nav>
     </div>
   );
--- src/pages/listOrders.tsx
+++ src/pages/listOrders.tsx
@@ -1,38 +1,52 @@
 import { useEffect, useState } from "react";
 import { Header } from "@/Components/header";
-import styles from '@/styles/OrderList.module.css'
+import styles from "@/styles/OrderList.module.css";
 import { buyTokens } from "./app";
-declare let window: any
+declare let window: any;
 export default function listOrders() {
-let makerAddress = "0x5dd7be3badc927a2dc355a276b7f6a3420550c96"
-	const [orders,setOrders] = useState([])
-	
-	useEffect(() => {
-	let availableOrders = window.localStorage.getItem("orderss") || [];
-	let availableOrdersObj = JSON.parse(`${availableOrders}`);
-		setOrders(availableOrdersObj);
-	},[])
-	console.log(orders)
-return(
-<>
-<Header/>
-<div className={styles.orders}>{orders.length == 0 ? <p>No orders available</p>: <>
-	{orders.map((order:any, index: any) => {
-return(
-		<div key={index} className={styles.list}>
-		<p><span>Maker Token: </span> {order.makerToken == makerAddress? "USDC" : "WETH"}</p>
-		<p><span>Taker Token: </span> {order.takerToken == makerAddress ? "USDC" : "WETH"}</p>
-		<p><span>Maker Amount: </span>{order.takerAmount}</p>	
-		<p><span>Taker Amount: </span>{order.makerAmount}</p>
-		<button onClick={buyTokens}>Buy</button>
-		
-</div>
-)
-	})}
+  let makerAddress = "0x5dd7be3badc927a2dc355a276b7f6a3420550c96";
+  const [orders, setOrders] = useState([]);
 
-</>}</div>
-</>
-)
-
-
-} 
+  useEffect(() => {
+    let availableOrders = window.localStorage.getItem("orderss") || [];
+    let availableOrdersObj = JSON.parse(`${availableOrders}`);
+    setOrders(availableOrdersObj);
+  }, []);
+  console.log(orders);
+  return (
+    <>
+      <Header />
+      <div className={styles.orders}>
+        {orders.length == 0 ? (
+          <p>No orders available</p>
+        ) : (
+          <>
+            {orders.map((order: any, index: any) => {
+              return (
+                <div key={index} className={styles.list}>
+                  <p>
+                    <span>Maker Token: </span>{" "}
+                    {order.makerToken == makerAddress ? "USDC" : "WETH"}
+                  </p>
+                  <p>
+                    <span>Taker Token: </span>{" "}
+                    {order.takerToken == makerAddress ? "USDC" : "WETH"}
+                  </p>
+                  <p>
+                    <span>Maker Amount: </span>
+                    {order.takerAmount}
+                  </p>
+                  <p>
+                    <span>Taker Amount: </span>
+                    {order.makerAmount}
+                  </p>
+                  <button onClick={buyTokens}>Buy</button>
+                </div>
+              );
+            })}
+          </>
+        )}
+      </div>
+    </>
+  );
+}
